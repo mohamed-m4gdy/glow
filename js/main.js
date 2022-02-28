19e5e7d4b906e2656b80d09a5f96ae3eb4f8de0d
xuncharted <evilonlyone@gmail.com>
2022-02-28 16:08:25 +0200

    Uploading

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Start Scroll to Top
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// End Scroll To Top
